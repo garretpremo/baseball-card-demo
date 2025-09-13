237aaf977f40688b921e3f6c57c321e11d2e4d1d
Garret Premo <gpremo@rationalenterprise.com>
2025-09-13 12:32:14 -0700

    Added a quick and dirty global navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-            <Link href="/players" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                Players
-            </Link>
-        </main>
-    </div>
+      <div
+          className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+          <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+              <Link href="/players"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                  View Baseball Player Stats Implementation
+              </Link>
+          </main>
+      </div>
   );
 }
