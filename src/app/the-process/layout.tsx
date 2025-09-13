1fd100f0d343517b57dee1f93b5ddb58489def64
Garret Premo <gpremo@rationalenterprise.com>
2025-09-13 13:52:49 -0700

    Additional updates to layout to improve scrolling

--- src/app/the-process/layout.tsx
+++ src/app/the-process/layout.tsx
@@ -1,14 +1,9 @@
 import Image from "next/image";
 
 export default function PageLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-    const styles: Record<string, string> = {
-        '--tw-prose-headings': 'white'
-    };
     return (
-        <div className="align-center">
-            <div className="prose" style={ styles }>
-                { children }
-            </div>
+        <div className="prose prose-invert">
+            { children }
         </div>
     );
 }
